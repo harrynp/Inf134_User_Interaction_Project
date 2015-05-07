8fb9a8159e20172b6173b489a3795e44c1ce29d4
Harry <harrynp12@gmail.com>
2015-05-06 22:07:37 -0700

    Changed friendster.com to go to index.php

--- web/photos.php
+++ web/photos.php
@@ -5,7 +5,7 @@ $u = "";
 if(isset($_GET["u"])){
 	$u = preg_replace('#[^a-z0-9]#i', '', $_GET['u']);
 } else {
-    header("location: http://www.friendster.com");
+    header("location: index.php");
     exit();
 }
 $photo_form = "";
